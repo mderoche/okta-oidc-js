681be05eafd7a0ca663d1fc4acdd4da7fe2d3a50
jmelberg <jordan.melberg@okta.com>
2018-09-28 11:10:22 -0700

    Remove unneeded constants from jwt-verifier

--- packages/jwt-verifier/test/constants.js
+++ packages/jwt-verifier/test/constants.js
@@ -15,12 +15,8 @@ const BASE_URI = process.env.BASE_URI || `http://localhost:${PORT}`;
 
 module.exports = {
   REDIRECT_URI: `${BASE_URI}/implicit/callback`,
-  LOGIN_PATH: `${BASE_URI}/login`,
-  LOGOUT_PATH: `${BASE_URI}/logout`,
-  PROTECTED_PATH: `${BASE_URI}/protected`,
   ISSUER: process.env.ISSUER,
   CLIENT_ID: process.env.SPA_CLIENT_ID || process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
   USERNAME: process.env.USERNAME,
   PASSWORD: process.env.PASSWORD,
   PORT,
