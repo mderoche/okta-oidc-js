7959d6912226b606fb5f5560c17ff338fe326d87
jmelberg <jordan.melberg@okta.com>
2018-09-28 11:06:23 -0700

    Remove unneeded constants from oidc-middleware

--- packages/oidc-middleware/test/e2e/util/constants.js
+++ packages/oidc-middleware/test/e2e/util/constants.js
@@ -15,9 +15,6 @@ const BASE_URI = process.env.BASE_URI || `http://localhost:${PORT}`;
 
 module.exports = {
   REDIRECT_URI: `${BASE_URI}/authorization-code/callback`,
-  LOGIN_PATH: `${BASE_URI}/login`,
-  LOGOUT_PATH: `${BASE_URI}/logout`,
-  PROTECTED_PATH: `${BASE_URI}/protected`,
   ISSUER: process.env.ISSUER,
   CLIENT_ID: process.env.WEB_CLIENT_ID || process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
